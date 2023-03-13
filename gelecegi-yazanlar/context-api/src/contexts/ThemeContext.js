85c2427ce4e4947eacab8d723f227346ed579da8
mrtSahin <mrtsvs07@gmail.com>
2023-03-13 21:56:58 +0300

    added localstorage

--- gelecegi-yazanlar/context-api/src/contexts/ThemeContext.js
+++ gelecegi-yazanlar/context-api/src/contexts/ThemeContext.js
@@ -1,19 +1,25 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const ThemeContext = createContext()
 
 export const ThemeContextProvider = ({ children }) => {
 
-    const [theme, setTheme] = useState('light')
+    const defaultTheme=localStorage.getItem('theme') ||'light'
 
-    const toggleTheme=()=>{ // Footer ve ChangeTheme componentlerinde aynı kodu çok kere yazmamak için burada bu fonksiyonu tanımladık vecontext ile ikisine de gönderdik.
-        setTheme(theme==='light'?'dark':'light')
+    const [theme, setTheme] = useState(defaultTheme)
+
+    const toggleTheme = () => { // Footer ve ChangeTheme componentlerinde aynı kodu çok kere yazmamak için burada bu fonksiyonu tanımladık vecontext ile ikisine de gönderdik.
+        setTheme(theme === 'light' ? 'dark' : 'light')
     }
 
     const values = {
         theme,
         toggleTheme
     }
+
+    useEffect(() => {
+        localStorage.setItem('theme', theme)
+    }, [theme])
 
     return <ThemeContext.Provider value={values}>{children}</ThemeContext.Provider>
 }                                 // artık veriyi App componentinden dağil de buradan sağlıyoruz.
